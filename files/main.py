5d5aa2b9fd8582e41cf251242c6fc056da85da31
Mordokrowka <48591214+Mordokrowka@users.noreply.github.com>
2020-07-20 16:49:31 +0200

    Update main.py
    Monitoring added and embedded into the main loop

--- files/main.py
+++ files/main.py
@@ -7,12 +7,15 @@ from tensorflow.keras.optimizers import Adam
 import numpy as np
 import uproot
 from sklearn.preprocessing import MinMaxScaler
+import matplotlib.pyplot as plt
+import math
 
 file = uproot.open("files\PhaseSpaceSimulation.root")
 events = file["PhaseSpaceTree"]
 NumEntries = events.numentries  #number of data objects (vectors)
 params = ["H1_PX","H1_PY","H1_PZ","H2_PX","H2_PY","H2_PZ","H3_PX","H3_PY","H3_PZ"]
 
+Mass_K = 493.677
 
 class GAN:
     def __init__(self):
@@ -90,6 +93,27 @@ class GAN:
         data = events.lazyarrays(params)
         data_arr = np.vstack(list(data[elem] for elem in params)).T
 
+        fig, ax = plt.subplots()
+        Mass_B = np.zeros(NumEntries)
+        for i in range(NumEntries):
+            p_products = np.array([np.sqrt(np.square(data_arr[i][0]) + np.square(data_arr[i][1]) + np.square(data_arr[i][2])),
+                       np.sqrt(np.square(data_arr[i][3]) + np.square(data_arr[i][4]) + np.square(data_arr[i][5])),
+                       np.sqrt(np.square(data_arr[i][6]) + np.square(data_arr[i][7]) + np.square(data_arr[i][8]))])
+            p_total = np.sqrt(np.square(data_arr[i][0]+data_arr[i][3]+data_arr[i][6]) +
+                       np.square(data_arr[i][1]+data_arr[i][4]+data_arr[i][7]) +
+                       np.square(data_arr[i][2]+data_arr[i][5]+data_arr[i][8]))
+            E_total = np.sqrt(np.square(p_products) + Mass_K ** 2)
+            Mass_B[i] = math.sqrt(np.sum(E_total) ** 2  - p_total ** 2)
+
+            #print(p_total)
+            #print(E_total)
+        n, bins, patches = ax.hist(Mass_B, 100, range =(5279,5279.3))
+        ax.set_xlabel('Mass of the B meson [MeV]')
+        ax.set_ylabel('Number of counts')
+        fig.savefig("images/InputData.png")
+        plt.close()
+
+
         # Rescale -1 to 1
         scaler = MinMaxScaler(feature_range=(-1, 1))
         scaler = scaler.fit(data_arr)
@@ -129,7 +153,43 @@ class GAN:
             # Plot the progress
             print("%d [D loss: %f, acc.: %.2f%%] [G loss: %f]" % (epoch, d_loss[0], 100*d_loss[1], g_loss))
 
+            # If at save interval => save generated image samples
+            if epoch % sample_interval == 0:
+                self.sample_images(epoch)
+
+    def sample_images(self, epoch):
+
+        r = 1000;
+        noise = np.random.normal(0, 1, (r , self.latent_dim))
+        gen_p = self.generator.predict(noise)
+        print(gen_p)
+
+        cnt = 0;
+        fig, ax = plt.subplots()
+        Mass_B = np.zeros(r)
+        for i in range(r):
+            p_products = np.array([np.sqrt(np.square(gen_p[i][0]) + np.square(gen_p[i][1]) + np.square(gen_p[i][2])),
+                       np.sqrt(np.square(gen_p[i][3]) + np.square(gen_p[i][4]) + np.square(gen_p[i][5])),
+                       np.sqrt(np.square(gen_p[i][6]) + np.square(gen_p[i][7]) + np.square(gen_p[i][8]))])
+            p_total = np.sqrt(np.square(gen_p[i][0]+gen_p[i][3]+gen_p[i][6]) +
+                       np.square(gen_p[i][1]+gen_p[i][4]+gen_p[i][7]) +
+                       np.square(gen_p[i][2]+gen_p[i][5]+gen_p[i][8]))
+            E_total = np.sqrt(np.square(p_products) + Mass_K ** 2)
+            Mass_B[i] = math.sqrt(np.sum(E_total) ** 2  - p_total ** 2)
+                #axs[i,j].imshow(gen_imgs[cnt, :,:,0], cmap='gray')
+                #axs[i,j].axis('off')
+
+            cnt += 1
+
+        n, bins, patches = ax.hist(Mass_B, 100)#, range =(5279,5279.3))
+        ax.set_xlabel('Mass of the B meson [MeV]')
+        ax.set_ylabel('Number of counts')
+        fig.savefig("images/%d.png" % epoch)
+        plt.close()
+
 
 if __name__ == '__main__':
+
+
     gan = GAN()
-    gan.train(epochs=30000, batch_size=128, sample_interval=200)
+    gan.train(epochs=3000, batch_size=128, sample_interval=200)
